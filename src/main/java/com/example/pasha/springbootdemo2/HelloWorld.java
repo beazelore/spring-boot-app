0e3c71a1af37b75b5979acc13180652bc4addd50
Pavlo Darchyn <pavlo.darchyn@gmail.com>
2018-09-28 18:55:38 +0300

    fix

--- src/main/java/com/example/pasha/springbootdemo2/HelloWorld.java
+++ src/main/java/com/example/pasha/springbootdemo2/HelloWorld.java
@@ -1,10 +1,8 @@
 package com.example.pasha.springbootdemo2;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class HelloWorld {
